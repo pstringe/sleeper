55b1a010c8159e3aa034d49e5ae5393774ac8f9a
Poitier Stringer <stringerpoitier@gmail.com>
2021-06-27 14:46:52 -0700

    gifs display on messages

--- src/Components/MessageList.js
+++ src/Components/MessageList.js
@@ -1,5 +1,7 @@
 import {
-    Grid, 
+    Grid,
+    GridList,
+    GridListTile,
     Typography,
     Avatar
 } from '@material-ui/core';
@@ -24,6 +26,10 @@ const useStyles = makeStyles({
     },
     name: {
         fontWeight: 'bold',
+    },
+    attatchment: {
+        width: '100%',
+        height: 'auto',
     }
 });
 
@@ -35,14 +41,23 @@ const MessageList = ({messages}) => {
             {messages.map((message, idx) => {
                 return (
                     <Grid className={classes.message} key={idx} item container direction='row'>
-                        <Avatar src={users.find((user) => user.id == message.userID).avatar} />
-                        <div className={classes.details}>
-                            <div className={classes.heading}>
+                        <Grid item xs={1}>
+                            <Avatar src={users.find((user) => user.id == message.userID).avatar} />
+                        </Grid>
+                        <Grid className={classes.details} item container xs={10} direction='column'>
+                            <Grid item className={classes.heading} direction='row'>
                                 <Typography className={classes.name} variant='subtitle1'>{message.userID}</Typography>
                                 <Typography variant='subtitle2'>{message.timestamp.toTimeString().split(' ')[0]}</Typography>
-                            </div>
-                            <Typography variant='body1'>{message.content.text}</Typography>
-                        </div>
+                            </Grid>
+                            <Typography variant='body1' align='left'>{message.content.text}</Typography>
+                            <Grid item container>
+                                {message.content.attatchments.map((gif, idx) => (
+                                    <Grid item xs={12} key={idx}>
+                                        <img className={classes.attatchment} src={gif}/>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Grid>
                     </Grid>
                 );
             })}
